8ec8d4f01ea83958107d297511106e3068c775d0
Anelechi edwin <anelechi@harrricdev.com.ng>
2017-02-11 21:27:07 +0100

    added monthly summary

--- resources/views/foundation/table.blade.php
+++ resources/views/foundation/table.blade.php
@@ -4,10 +4,21 @@
                 <tr>
                     <th>Id</th>
                     <th>Name</th>
-                    <th>Email Address</th>
                     <th>Present Percentage</th>
                     <th>Late Percentage</th>
                     <th>Early Percentage</th>
+                    <th>Jan</th>
+                    <th>Feb</th>
+                    <th>Mar</th>
+                    <th>April</th>
+                    <th>May</th>
+                    <th>June</th>
+                    <th>July</th>
+                    <th>august</th>
+                    <th>sept</th>
+                    <th>Oct</th>
+                    <th>Nov</th>
+                    <th>Dec</th>
                 </tr>
             </thead>
             <tbody>
@@ -16,9 +27,20 @@
                         <tr>
                             <th>{{ $employee->id }}</a></th>
                             <th><a href="{{ route('single', ['employee'=> $employee->id]) }}">{{ $employee->name }}<a href="#"></th>
-                            <th>{{ $employee->email }}</th>
                             <th>{{ round(($employee->days_present/54)*100, 1) }} %</th>
                             <th>{{ round(($employee->days_late/$employee->days_present)*100, 1) }} %</th>                                       <th>{{ round(($employee->days_early/$employee->days_present)*100, 1) }} %</th>
+                            <th>{{ round(($employee->jan/31)*100, 1) }} %</th>
+                            <th>{{ round(($employee->feb/29)*100, 1) }} % </th>
+                            <th>{{ round(($employee->march/31)*100, 1) }} % </th>
+                            <th>{{ round(($employee->april/30)*100, 1) }} % </th>
+                            <th>{{ round(($employee->may/31)*100, 1) }} % </th>
+                            <th>{{ round(($employee->june/30)*100, 1) }} % </th>
+                            <th>{{ round(($employee->july/31)*100, 1) }} % </th>
+                            <th>{{ round(($employee->august/31)*100, 1) }} % </th>
+                            <th>{{ round(($employee->september/30)*100, 1) }} % </th>
+                            <th>{{ round(($employee->october/31)*100, 1) }} % </th>
+                            <th>{{ round(($employee->november/30)*100, 1) }} % </th>
+                            <th>{{ round(($employee->december/31)*100, 1) }} % </th>
                         </tr>
                     
                 @endforeach
